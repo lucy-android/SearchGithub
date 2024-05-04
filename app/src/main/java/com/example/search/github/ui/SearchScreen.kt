8febea600c3a79c101b1ccf99460f15f10a33361
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-05-04 20:20:11 +0300

    SG-3: load data from server

--- app/src/main/java/com/example/search/github/ui/SearchScreen.kt
+++ app/src/main/java/com/example/search/github/ui/SearchScreen.kt
@@ -1,17 +1,33 @@
 package com.example.search.github.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.example.search.github.R
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.search.github.domain.model.GithubRepositoryDomain
+import com.example.search.github.ui.viewmodel.GithubReposViewModel
 
 @Composable
-fun SearchScreen() {
-    Column {
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = stringResource(R.string.search_screen))
+fun SearchScreen(viewModel: GithubReposViewModel = hiltViewModel()) {
+
+    viewModel.loadData()
+
+    val repoPagingItems: LazyPagingItems<GithubRepositoryDomain> =
+        viewModel.state.collectAsLazyPagingItems()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        items(repoPagingItems.itemCount){ index ->
+            val repo = repoPagingItems[index]
+            if (repo != null) {
+                Text(repo.fullName)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/search/github/ui/viewmodel/GithubReposViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.search.github.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.search.github.domain.model.GithubRepositoryDomain
+import com.example.search.github.domain.usecase.FetchReposUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GithubReposViewModel @Inject constructor(private val fetchReposUseCase: FetchReposUseCase) :
+    ViewModel() {
+
+    private val _state: MutableStateFlow<PagingData<GithubRepositoryDomain>> =
+        MutableStateFlow(PagingData.empty())
+    val state: StateFlow<PagingData<GithubRepositoryDomain>> = _state
+
+    fun loadData() {
+        viewModelScope.launch(Dispatchers.Default) {
+            fetchReposUseCase.fetchRepos("droid").distinctUntilChanged().cachedIn(viewModelScope)
+                .collectLatest {
+                    _state.value = it
+                }
+        }
+    }
+}
