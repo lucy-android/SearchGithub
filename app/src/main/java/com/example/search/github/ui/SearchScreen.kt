9ce2f3da8d354aedfbf2dad8bf142a27809cf926
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-05-05 23:26:53 +0300

    SG-3: add text field to the search screen

--- app/src/main/java/com/example/search/github/ui/SearchScreen.kt
+++ app/src/main/java/com/example/search/github/ui/SearchScreen.kt
@@ -1,10 +1,16 @@
 package com.example.search.github.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -19,14 +25,19 @@ fun SearchScreen(viewModel: GithubReposViewModel = hiltViewModel()) {
     val repoPagingItems: LazyPagingItems<GithubRepositoryDomain> =
         viewModel.state.collectAsLazyPagingItems()
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        items(repoPagingItems.itemCount){ index ->
-            val repo = repoPagingItems[index]
-            if (repo != null) {
-                Text(repo.fullName)
+    val textFieldValue = remember { mutableStateOf("") }
+
+    Column {
+        OutlinedTextField(value = textFieldValue.value, onValueChange = { textFieldValue.value = it })
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp)
+        ) {
+            items(repoPagingItems.itemCount) { index ->
+                val repo = repoPagingItems[index]
+                if (repo != null) {
+                    Text(repo.fullName)
+                }
             }
         }
     }
