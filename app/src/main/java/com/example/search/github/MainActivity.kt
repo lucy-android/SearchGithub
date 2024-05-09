9c672f3b6b6d2ca7e55cf6411749202ed53b303d
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-05-09 17:17:48 +0300

    SG-3: remove redundant method call from MainActivity.kt

--- app/src/main/java/com/example/search/github/MainActivity.kt
+++ app/src/main/java/com/example/search/github/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.search.github
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,21 +8,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.search.github.data.datasource.GithubDataSource
-import com.example.search.github.remote.GithubApi
 import com.example.search.github.ui.SearchApp
 import com.example.search.github.ui.theme.SearchGithubTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var githubDataSource: GithubDataSource
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -31,10 +21,6 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        GlobalScope.launch {
-            Log.d("APP_TAG", "onCreate: ${githubDataSource.getRepositories("abc", 1)}")
-
-        }
         setContent {
             SearchGithubTheme {
                 Surface(
