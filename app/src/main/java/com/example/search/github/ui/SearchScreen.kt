98fcac14340c24709f541ef5ca54708e0cfeb019
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-05-15 07:54:00 +0300

    SG-3: center progress indicator, extract string resource

--- app/src/main/java/com/example/search/github/ui/SearchScreen.kt
+++ app/src/main/java/com/example/search/github/ui/SearchScreen.kt
@@ -1,7 +1,9 @@
 package com.example.search.github.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,9 +22,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
@@ -58,22 +62,26 @@ fun SearchScreen(viewModel: GithubReposViewModel = hiltViewModel()) {
             items(repoPagingItems.itemCount) { index ->
                 val repo = repoPagingItems[index]
                 if (repo != null) {
-                    RepoItem(repo = repo)
+                    RepoItem(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 20.dp), repo = repo
+                    )
                 }
             }
             item {
-                if(loadState.append is LoadState.Loading){
-                    CircularProgressIndicator()
+                if (loadState.append is LoadState.Loading) {
+                    CircularProgressBar()
                 }
 
                 if (loadState.refresh is LoadState.Loading && textFieldValue.isNotEmpty()) {
-                    CircularProgressIndicator()
-                } else if(loadState.refresh is LoadState.Loading){
+                    CircularProgressBar()
+                } else if (loadState.refresh is LoadState.Loading) {
                     EmptyResult(
                         modifier = Modifier
                             .padding(vertical = dimensionResource(id = R.dimen.dp10))
                             .fillParentMaxSize(),
-                        text = "Type something"
+                        text = stringResource(R.string.type_something)
                     )
                 }
                 when (val refresh = loadState.refresh) {
@@ -93,13 +101,14 @@ fun SearchScreen(viewModel: GithubReposViewModel = hiltViewModel()) {
 }
 
 @Composable
-fun RepoItem(repo: GithubRepo) {
+fun RepoItem(modifier: Modifier = Modifier, repo: GithubRepo) {
     Column {
         Row {
-            Text(modifier = Modifier.weight(1f), text = repo.fullName)
+            Text(modifier = modifier.weight(1f).align(Alignment.CenterVertically), text = repo.fullName)
             FilledIconButton(
                 modifier = Modifier
                     .padding(start = dimensionResource(id = R.dimen.dp5))
+                    .align(Alignment.CenterVertically)
                     .size(dimensionResource(id = R.dimen.dp30)),
                 onClick = {
                     // TODO download action
@@ -119,6 +128,20 @@ fun RepoItem(repo: GithubRepo) {
 }
 
 @Composable
-fun EmptyResult(modifier: Modifier = Modifier, text: String = "No results were obtained") {
-    Text(text = text)
+fun EmptyResult(
+    modifier: Modifier = Modifier,
+    text: String = stringResource(R.string.no_results_were_obtained)
+) {
+    Text(modifier = modifier, text = text)
+}
+
+@Composable
+fun CircularProgressBar(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .padding(vertical = dimensionResource(id = R.dimen.dp10))
+            .fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(modifier)
+    }
 }
